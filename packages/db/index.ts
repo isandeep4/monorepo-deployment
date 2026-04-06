a2fa8a299fac99da61b1add26a8acc1c48c6a19f
isandeep4 <isandeepsahoo5@gmail.com>
2026-04-05 21:45:49 -0600

    integrated with DB

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -1,8 +1,12 @@
+import "dotenv/config";
 import { PrismaClient } from "./generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
+import { Pool } from "pg";
 
 const connectionString = process.env.DATABASE_URL;
-const adapter = new PrismaPg({
+const pool = new Pool({
   connectionString,
 });
+console.log("Using database connection string:", connectionString);
+const adapter = new PrismaPg(pool);
 export const prismaClient = new PrismaClient({ adapter });
